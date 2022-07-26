2e303a5c9763675825bd90282de89458a2bb7332
gemmadkkim <gemmaer81@naver.com>
2022-07-26 18:08:38 +0900

    0726

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -17,23 +17,33 @@ $(".toggle").click(function(){
     }
 })
 
-
-
 $(".mMenu li>a").click(function(e){
     e.preventDefault();
     console.log($(this).parent().find(".submenu").height())
 
-    let subHeight = $(this).parent().find(".submenu").height();
-    //$(".mMenu li>a").parent().find(".submenu").slideUp();
-    $(".mMenu li").css("height","initial")
+    // let subHeight = $(this).parent().find(".submenu").height();
+    $(".mMenu li>a").parent().find(".submenu").slideUp();
+    // $(".mMenu li").css("height","initial")
     if($(this).hasClass("active")){
         $(".mMenu li>a").removeClass("active");
-        $(".mMenu li").css("height","initial")
+        $(".mMenu li").css("height","initial");
     }else{
-        // $(this).parent().find(".submenu").slideDown();
+        $(this).parent().find(".submenu").slideDown();
         $(".mMenu li>a").removeClass("active");
-        $(this).parent().css("height",subHeight)
+        // $(this).parent().css("height",subHeight)
         $(this).addClass("active");
     }
 })
 
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+});
