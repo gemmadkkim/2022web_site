490399b45c0fbd036f0a4913412e9acf02e9bb60
gemmadkkim <gemmaer81@naver.com>
2022-09-20 18:05:44 +0900

    0920 jjf

---
+++ 11jazzfestival/js/data.js
@@ -0,0 +1,12 @@
+const mydata = [
+    {
+        en:"Joey Alexander Trio ",
+        kn:"조이 알렉산더 트리오",
+        img:""
+    },
+    {
+        en:"Joey Alexander Trio ",
+        kn:"조이 알렉산더 트리오",
+        img:""
+    },
+]
