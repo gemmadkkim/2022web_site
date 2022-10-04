050bcec09cd3d47042405e2a43522347f1bcb141
gemmadkkim <gemmaer81@naver.com>
2022-10-04 16:50:53 +0900

    1004 prg

--- 11jazzfestival/js/data.js
+++ 11jazzfestival/js/data.js
@@ -7,129 +7,129 @@ const pList01= [
     },
     {
         index:2,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"아르코홀릭",
+        en:"ARCO-HOLIC",
+        img:"arco.png"
     },
     {
         index:3,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"아비샤이 코헨 퀄텟",
+        en:"Avishai Cohen Quartet",
+        img:"avishai.jpg"
     },
     {
         index:4,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"바렌시아",
+        en:"Barencia",
+        img:"barencia.png"
     },
     {
         index:5,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"아가타보헤미안 베티아스",
+        en:"Bohemian Betyars",
+        img:"bohemian.png"
     },
     {
         index:6,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"다니엘 가르시아 ",
+        en:"Daniel García",
+        img:"daniel.jpg"
     },
     {
         index:7,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"다니엘 가르시아 트리오",
+        en:"Daniel García Trio",
+        img:"daniel_trio.png"
     },
   
 ]
 const pList02 = [
     {
         index:1,
-        ko:"아르코",
-        en:"Arco",
-        img:"arco.png"
+        ko:"딥마인드 [박제신, 이영우, 김종국, 송준호, 안상준] ",
+        en:"Deep Mind [Jaeshin Park, Youngwoo Lee, Jongkuk Kim, Junho Song, Sangjun Ahn]",
+        img:"deepmind01.png"
     },
     {
         index:2,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"욘 DJ Set",
+        en:"DJ Set JON DJ Set ",
+        img:"jon.png"
     },
     {
         index:3,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"이에이비에스",
+        en:"EABS",
+        img:"eabs.png"
     },
     {
         index:4,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"에스터 라다",
+        en:"Ester Rada",
+        img:"ester.png"
     },
     {
         index:5,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"유레카 밴드",
+        en:"Eureka Band",
+        img:"eureka.jpg"
     },
     {
         index:6,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"파이브 포인츠",
+        en:"Five Points",
+        img:"fivepoints.png"
     },
     {
         index:7,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"이지혜 트리오 feat. 전송이 ",
+        en:"Gee Hye Lee Trio feat. Song Yi Jeon",
+        img:"leesong.png"
     },
   
 ]
 const pList03 = [
     {
         index:1,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"조해인 퀸텟",
+        en:"Hae In Cho Quintet",
+        img:"haein.png"
     },
     {
         index:2,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"한스 니스반트",
+        en:"Hans Nieswandt ",
+        img:"hans.png"
     },
     {
         index:3,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"하드피아노",
+        en:"HARDPIANO",
+        img:"hardpiano.png"
     },
     {
         index:4,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"재즈미어 혼",
+        en:"Jazzmeia Horn",
+        img:"horn.png"
     },
     {
         index:5,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"이지민 퀄텟",
+        en:"Jimin Lee Quartet ",
+        img:"jimin.png"
     },
     {
         index:6,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"호아나 고밀라 & 라이아 바예스 ",
+        en:"Joana Gomila & Laia Vallès",
+        img:"joana.jpg"
     },
     {
         index:7,
-        ko:"아가타",
-        en:"AGATHA",
-        img:"agatha.jpg"
+        ko:"김현철",
+        en:"KIM HYUN CHUL ",
+        img:"kim.jpg"
     },
   
 ]
--- 11jazzfestival/js/script.js
+++ 11jazzfestival/js/script.js
@@ -27,10 +27,10 @@ $(document).ready(function () {
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
