449fa5e837054ca0542a9e3e4257c6c612056484
gemmadkkim <gemmaer81@naver.com>
2022-10-06 18:14:50 +0900

    1006 footer

--- 11jazzfestival/js/script.js
+++ 11jazzfestival/js/script.js
@@ -1,10 +1,10 @@
 $("#fullpage").fullpage({
   menu: ".nav",
   anchors: ["home", "lineup", "ticket", "program", "notice"],
-  navigation: true,
-  navigationPosition: "fp-left",
-  navigationTooltips: ["home", "lineup", "ticket", "program", "notice"],
-  responsiveWidth: 900
+  // navigation: true,
+  // navigationPosition: "fp-left",
+  // navigationTooltips: ["home", "lineup", "ticket", "program", "notice"],
+  responsiveWidth: 900,
 });
 
 
